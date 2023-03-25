f3361375fdefd1c2aa4bbe489e194dcf087ccab9
Seth Lim <sethlimjs@gmail.com>
2023-03-25 02:19:34 -0700

    initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],# added BASE_DIR
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
